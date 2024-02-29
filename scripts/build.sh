1158eeb01f1c9a119ffe7cabd8baa8786429373e
Andrew Mushel <almushel@protonmail.com>
2024-02-29 12:41:14 -0600

    Option flags and filename handling

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SRC_DIR=$SCRIPT_DIR/../src
 BIN_DIR=$SCRIPT_DIR/../bin
 
-if [ -d $BIN ]; then 
+if [ ! -d $BIN_DIR ]; then 
 	mkdir $BIN_DIR
 fi
 
--- src/main.c
+++ src/main.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define array_len(arr) sizeof(arr) / sizeof(arr[0])
+
+typedef enum WC_Print_Options {
+	OPTION_BYTE_COUNT = 1,	// -c, --bytes
+	OPTION_CHAR_COUNT,	// -m, --chars
+	OPTION_LINE_COUNT,	// -l, --lines
+	OPTION_WORD_COUNT,	// -w, --words
+	OPTION_LINE_LENGTH	// -L, --max-line-length
+} WC_Print_Options;
 
 long unsigned int strlen(const char* s) {
 	if (s[0] == '\0') {
@@ -20,9 +31,15 @@ long unsigned int strlen(const char* s) {
 }
 
 int main(int argc, char* argv[]) {
-	for (int i = 0; i < argc; i++) {
-		printf("strlen(\"%s\"): %u\n", argv[i], (unsigned int)strlen(argv[i]));
+//	for (int i = 0; i < argc; i++) {
+//		printf("strlen(\"%s\"): %u\n", argv[i], (unsigned int)strlen(argv[i]));
 	}
+
+	WC_Print_Options options;	
+
+	size_t fn_cap = 8;
+	size_t fn_len = 0;
+	char** file_names = malloc(sizeof(char*) * fn_cap);
 
 	if (argc == 1) {
 		// Attempt to read from stdin
@@ -31,6 +48,8 @@ int main(int argc, char* argv[]) {
 		for (int i = 1; i < argc; i++) {
 			char* arg = argv[i];
 			size_t len = strlen(arg);
+
+			// printf("%s", arg);
 			
 			if (arg[0] == '-') {
 				if (arg[1] == '-') {
@@ -40,24 +59,57 @@ int main(int argc, char* argv[]) {
 				// --lines
 				// --max-line-length
 				// --words
-				} else if (len == 2){
+				} else if (len == 2) {
 				// Handle '-x' options	
 					switch(arg[1]) {
 						case 'c':
+							options |= OPTION_BYTE_COUNT;
+							break;
 						case 'm':
+							options |= OPTION_CHAR_COUNT;
+							break;
 						case 'l':
+							options |= OPTION_LINE_COUNT;
+							break;
 						case 'L':
+							options |= OPTION_LINE_LENGTH;
+							break;
 						case 'w':
+							options |= OPTION_WORD_COUNT;
+							break;
 						default:
 							//Invalid option
 							break;
 					}
-				} else {
-					// Invalid option
+				} 
+			} else {
+				if(fn_len >= fn_cap) {
+					fn_cap *= 2;
+					char** fn_new = realloc(file_names, sizeof(char*) * fn_cap);
+					if (fn_new) {
+						file_names = fn_new;
+					}
 				}
+				file_names[fn_len++] = arg;
 			}
 		}
 	}
+
+	printf("filenames: ");
+	for (int i = 0; i < fn_len; i++) {
+				
+		if (option & OPTION_LINE_COUNT) {}
+		if (option & OPTION_WORD_COUNT) {}
+		if (option & OPTION_CHAR_COUNT) {}
+		if (option & OPTION_BYTE_COUNT) {
+
+		}
+		if (option & OPTION_LINE_LENGTH) {}
+		
+		printf("%s, ", file_names[i]);
+	}
+	printf("\n");
+
 
 	return 0;
 }
